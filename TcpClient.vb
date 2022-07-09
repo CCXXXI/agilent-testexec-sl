fa400317d99b870a86dae6e42374cc2e70d9ea1c
CCXXXI <ccxxxi@qq.com>
2022-07-09 11:27:53 +0800

    refactor: simplify TcpClient

--- TcpClient.vb
+++ TcpClient.vb
@@ -11,30 +11,16 @@ Public Class TcpClient
     End Sub
 
     Public Function Check() As Boolean
-        If _networkStream.CanWrite And _networkStream.CanRead Then
-            ' Do a simple write.
-            Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes("test")
-            _networkStream.Write(sendBytes, 0, sendBytes.Length)
-            ' Read the NetworkStream into a byte buffer.
-            Dim bytes(_tcpClient.ReceiveBufferSize) As Byte
-            _networkStream.Read(bytes, 0, CInt(_tcpClient.ReceiveBufferSize))
-            ' todo: check
-            Dim response As String = Encoding.ASCII.GetString(bytes)
-            Return String.Compare(response, "test") = 0
-        Else
-            If Not _networkStream.CanRead Then
-                Console.WriteLine("cannot not write data to this stream")
-                _tcpClient.Close()
-            Else
-                If Not _networkStream.CanWrite Then
-                    Console.WriteLine("cannot read data from this stream")
-                    _tcpClient.Close()
-                End If
-            End If
-        End If
-    End Function
+        ' write
+        Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes("test")
+        _networkStream.Write(sendBytes, 0, sendBytes.Length)
 
-    Public Sub Close()
-        _tcpClient.Close()
-    End Sub
+        ' read
+        Dim bytes(_tcpClient.ReceiveBufferSize) As Byte
+        _networkStream.Read(bytes, 0, CInt(_tcpClient.ReceiveBufferSize))
+
+        ' check
+        Dim response As String = Encoding.ASCII.GetString(bytes)
+        Return String.Compare(response, "test") = 0
+    End Function
 End Class
