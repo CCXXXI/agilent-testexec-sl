ae228d78e26624a551b2b057e68dfec74b09c76f
CCXXXI <ccxxxi@qq.com>
2022-07-11 17:22:17 +0800

    feat: something after test

--- TcpClient.vb
+++ TcpClient.vb
@@ -1,10 +1,13 @@
-﻿Imports System.Net.Sockets
+﻿Imports System.Linq
+Imports System.Net.Sockets
 
 Public Class TcpClient
     Const Host As String = "localhost"
     Const Port As Integer = 8000
     ReadOnly _readCmd As Byte() = New Byte() {&HFE, &H2, &H0, &H0, &H0, &H8, &H6D, &HC3}
     Const YesCode As Byte = &H1
+    ReadOnly _report As Byte() = New Byte() {&HFE, &H5, &H0, &H0, &HFF, &H0, &H98, &H35}
+    ReadOnly _expectResponse As Byte() = New Byte() {&HFE, &H5, &H0, &H0, &HFF, &H0, &H98, &H35}
 
     ReadOnly _tcpClient As New Net.Sockets.TcpClient()
     Dim _networkStream As NetworkStream
@@ -33,5 +36,18 @@ Public Class TcpClient
 
         ' check
         Return bytes(3) = YesCode
+    End Function
+
+    Public Function Report() As Boolean
+        _networkStream.Write(_report, 0, _report.Length)
+
+        Dim bytes(_tcpClient.ReceiveBufferSize) As Byte
+        _networkStream.Read(bytes, 0, CInt(_tcpClient.ReceiveBufferSize))
+
+        Return bytes.SequenceEqual(_expectResponse)
+    End Function
+
+    Public Function Connected() As Boolean
+        Return _tcpClient.Connected
     End Function
 End Class
